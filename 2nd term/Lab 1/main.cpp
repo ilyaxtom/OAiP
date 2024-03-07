2da6bbf0f7937267877301e27fba128a2d338c93
xtom_linux <xtom38@gmail.com>
2024-03-07 14:20:43 +0300

    lab 1 term 2

--- 2nd term/Lab 1/main.cpp
+++ 2nd term/Lab 1/main.cpp
@@ -2,27 +2,47 @@
 
 using namespace std;
 
-int fibonachiNumber(int n);
+int fibRec(int n) {
+    if (n == 1 || n == 2)
+        return 1;
+    else
+        return fibRec(n - 1) + fibRec(n - 2);
+}
 
-int main() {
-    for (;;) {
-        int num;
-        cout << "Введите номер: ";
-        cin >> num;
-
-        if (num == 0) {
-            break;
+int fib(int n) {
+    int fn_2 = 0, fn_1 = 1, f;
+    
+    if (n == 1)
+        return 1;
+    else {
+        for (int i = 2; i <= n; i++) {
+            f = fn_2 + fn_1;
+            fn_2 = fn_1;
+            fn_1 = f;
         }
-
-        cout << "Число под номером " << num << ": " << fibonachiNumber(num) << '\n';
-        cout << '\n';
+        return fn_1;
     }
 }
 
-int fibonachiNumber(int n) {
-    if (n == 1 || n == 2) {
-        return 1;
-    } else {
-        return fibonachiNumber(n - 2) + fibonachiNumber(n - 1);
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    int n;
+
+    for (;;) {
+        cout << "Введите n: ";
+        cin >> n;
+
+        if (n == 0)
+            break;
+        else {
+            while (n < 1)
+                cin >> n;
+            cout << "Fb(n) = " << fib(n) << '\n';
+            cout << "Rec Fb(n) = " << fibRec(n) << '\n';
+
+            cout << '\n';
+        }
     }
 }
